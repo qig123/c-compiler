437177ceb8e08156773e4f4841819b5576a9217b
qig123 <694546897@qq.com>
2024-04-17 07:51:32 +0800

    加法和减法

--- 9cc.c
+++ 9cc.c
@@ -1,13 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main(int argc, char const* argv[]) {
+int main(int argc, char* argv[]) {
     if (argc != 2) {
         fprintf(stderr, "参数数量无效\n");
     }
+    char* p = argv[1];
+
     printf(".intel_syntax noprefix\n");
     printf(".globl main\n");
     printf("main:\n");
-    printf("  mov rax, %d\n", atoi(argv[1]));
+    printf("  mov rax, %ld\n", strtol(p, &p, 10));
+
+    while (*p) {
+        if (*p == '+') {
+            p++;
+            printf("  add rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        if (*p == '-') {
+            p++;
+            printf("  sub rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+
+        fprintf(stderr, "予期しない文字です: '%c'\n", *p);
+        p++;
+    }
+
     printf("  ret\n");
     return 0;
 }
